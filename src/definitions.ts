105fc7412a88185290c387906bdf9177b90a5c9e
Rubén Panadero <rpanadero@okode.com>
2019-07-09 10:55:56 +0200

    Added event listener methods

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,11 @@
+import { PluginListenerHandle } from "@capacitor/core";
+
+export interface RegistrationToken {
+  token: string;
+}
+export interface VoipNotification {
+  data: any;
+}
 declare module "@capacitor/core" {
   interface PluginRegistry {
     Voip: VoipPlugin;
@@ -6,4 +14,7 @@ declare module "@capacitor/core" {
 
 export interface VoipPlugin {
   echo(options: { value: string }): Promise<{value: string}>;
+  addListener(eventName: 'registration', listenerFunc: (res: RegistrationToken) => void): PluginListenerHandle;
+  addListener(eventName: 'registrationError', listenerFunc: () => void): PluginListenerHandle;
+  addListener(eventName: 'pushNotificationReceived', listenerFunc: (notification: VoipNotification) => void): PluginListenerHandle;
 }
